e65b20c4e45327286ae59aec4e39c19107ae2b24
Julie Nisbet <jnisbet@appacademy.io>
2020-12-07 15:29:24 -0800

    adding an npm package

--- gitIsCool.js
+++ gitIsCool.js
@@ -1,2 +1,4 @@
-console.log("Git is soo awesome")
-console.log("Git is the best for realz")
+var colors = require('colors');
+
+console.log("Git is soo awesome".zebra)
+console.log("Git is the best for realz".rainbow)
